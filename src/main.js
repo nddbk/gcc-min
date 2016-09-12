ebaf8ace5279b0d02bd66613f593fae037a0d4d6
Dong Nguyen <ndaidong@gmail.com>
2016-09-12 16:47:16 +0700

    Add more test cases

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ var path = require('path');
 var exec = require('child_process').execSync;
 var request = require('request');
 var bella = require('bellajs');
+var Promise = require('promise-wtf');
 
 const API = 'https://closure-compiler.appspot.com/compile';
 
@@ -69,25 +70,29 @@ var send = (source) => {
   });
 };
 
-var minify = (file, output) => {
+var minify = (file, output, pkg = {}) => {
   let s = fs.readFileSync(file);
-  send(s).then((json) => {
+  return send(s).then((json) => {
     if (json.errors) {
       log.error(json.errors);
     } else if (json.warnings) {
       log.warn(json.warnings);
     } else {
-      let repo = pack.repository;
+      let repo = pkg.repository || pack.repository;
+      let auth = pkg.author || pack.author;
+      let lice = pkg.license || pack.license;
+      let name = pkg.name || pack.name;
+      let vers = pkg.version || pack.version;
       let date = bella.date;
       let sd = date.utc();
       let x = [
         `/**`,
-        ` * ${pack.name}`,
-        ` * v${pack.version}`,
+        ` * ${name}`,
+        ` * v${vers}`,
         ` * built: ${sd}`,
         ` * ${repo.type}: ${repo.url}`,
-        ` * author: ${pack.author}`,
-        ` * License: ${pack.license}`,
+        ` * author: ${auth}`,
+        ` * License: ${lice}`,
         `**/`,
         `;${json.compiledCode}`
       ].join('\n');
@@ -100,15 +105,20 @@ var minify = (file, output) => {
 };
 
 
-var prepare = (input, output) => {
+var prepare = (input, output, pkg = {}) => {
+  console.log(input);
   if (!fs.existsSync(input)) {
+    console.log('Input not found');
     return false;
   } else if (!path.extname(input)) {
     input += '/main.js';
   }
+  console.log(pkg);
+
+  let name = pkg.name || pack.name;
 
   if (!path.extname(output)) {
-    output += `/${pack.name}.min.js`;
+    output += `/${name}.min.js`;
   }
   let dir = path.dirname(output);
   exec('rm -rf ' + dir);
@@ -117,8 +127,19 @@ var prepare = (input, output) => {
   let i = path.normalize(input);
   let o = path.normalize(output);
 
-  return minify(i, o);
+  return minify(i, o, pkg);
 };
+
+prepare('../test/data/a.js', '../test/data/b.js', {
+  name: 'gcc-test',
+  version: '1.2.3',
+  repository: {
+    type: 'git',
+    url: 'https://github.com/ndaidong/gcc-min'
+  },
+  author: '@ndaidong',
+  license: 'MIT'
+});
 
 module.exports = {
   minify,
