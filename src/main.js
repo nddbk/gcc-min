4f7de8b84d3ec0a753035b8ec143716041f9aad6
Dong Nguyen <ndaidong@gmail.com>
2016-09-13 11:47:47 +0700

    Use google-closure-compiler-js module

--- src/main.js
+++ src/main.js
@@ -6,78 +6,49 @@
 var fs = require('fs');
 var path = require('path');
 var exec = require('child_process').execSync;
-var request = require('request');
 var bella = require('bellajs');
 var Promise = require('promise-wtf');
 
-const API = 'https://closure-compiler.appspot.com/compile';
+var compiler = require('google-closure-compiler-js');
 
 const LEVEL = 'SIMPLE_OPTIMIZATIONS';
-const INFO = ['compiled_code', 'warnings', 'errors'];
-const OUTPUT = 'json';
-
-var log = {
-  error: (msg) => {
-    console.log('\x1b[31m', msg);
-  },
-  warn: (msg) => {
-    console.log('\x1b[33m', msg);
-  },
-  success: (msg) => {
-    console.log('\x1b[32m', msg);
-  }
-};
+const LANG_IN = 'ES6';
+const LANG_OUT = 'ES5';
 
 var pack;
 
 try {
   pack = require('../../../package');
 } catch (e) {
-  log.warn('Not found top package info. Use local.');
   pack = require('../package');
   pack.__e__ = e;
 }
 
-var send = (source) => {
-  return new Promise((resolve, reject) => {
-    let info = '';
-    let arr = [];
-    INFO.forEach((item) => {
-      arr.push(`output_info=${item}`);
-    });
-    info = arr.join('&');
-    let target = `
-      ${API}?compilation_level=${LEVEL}&output_format=${OUTPUT}&${info}
-    `;
-    let data = {
-      js_code: source // eslint-disable-line camelcase
-    };
+var handle = (file, output, pkg = {}) => {
 
-    return request.post(target, {form: data}, (err, response, body) => {
-      if (err) {
-        return reject(err);
-      }
-      try {
-        let o = JSON.parse(body);
-        if (!o) {
-          return reject(new Error('Invalid JSON format'));
+  let s = '';
+  let r = {};
+
+  return Promise.series([
+    (next) => {
+      fs.readFile(file, 'utf8', (err, content) => {
+        if (err) {
+          console.log(err);
         }
-        return resolve(o);
-      } catch (e) {
-        return reject(e);
-      }
-    });
-  });
-};
-
-var minify = (file, output, pkg = {}) => {
-  let s = fs.readFileSync(file);
-  return send(s).then((json) => {
-    if (json.errors) {
-      log.error(json.errors);
-    } else if (json.warnings) {
-      log.warn(json.warnings);
-    } else {
+        s = content;
+        next();
+      });
+    },
+    (next) => {
+      r = compiler.compile({
+        compilationLevel: LEVEL,
+        languageIn: LANG_IN,
+        languageOut: LANG_OUT,
+        jsCode: [{src: s}]
+      });
+      next();
+    },
+    (next) => {
       let repo = pkg.repository || pack.repository;
       let auth = pkg.author || pack.author;
       let lice = pkg.license || pack.license;
@@ -85,7 +56,8 @@ var minify = (file, output, pkg = {}) => {
       let vers = pkg.version || pack.version;
       let date = bella.date;
       let sd = date.utc();
-      let x = [
+
+      r.fileContent = [
         `/**`,
         ` * ${name}`,
         ` * v${vers}`,
@@ -94,54 +66,46 @@ var minify = (file, output, pkg = {}) => {
         ` * author: ${auth}`,
         ` * License: ${lice}`,
         `**/`,
-        `;${json.compiledCode}`
+        `;${r.compiledCode}`
       ].join('\n');
-      fs.writeFileSync(output, x, 'utf8');
-      log.success('File has been minified');
+      next();
+    },
+    (next) => {
+      fs.writeFile(output, r.fileContent, 'utf8', (err) => {
+        if (err) {
+          console.log(err);
+        }
+        next();
+      });
     }
-  }).catch((err) => {
-    log.error(err);
+  ]);
+};
+
+
+var compile = (input, output, pkg = {}) => {
+  return new Promise((resolve, reject) => {
+    if (!fs.existsSync(input)) {
+      return reject(new Error(`${input} could not be found.`));
+    }
+    if (!path.extname(input)) {
+      input += '/main.js';
+    }
+    let name = pkg.name || pack.name;
+
+    if (!path.extname(output)) {
+      output += `/${name}.min.js`;
+    }
+    let dir = path.dirname(output);
+    exec('rm -rf ' + dir);
+    exec('mkdir ' + dir);
+
+    let i = path.normalize(input);
+    let o = path.normalize(output);
+
+    return resolve(handle(i, o, pkg));
   });
 };
 
-
-var prepare = (input, output, pkg = {}) => {
-  console.log(input);
-  if (!fs.existsSync(input)) {
-    console.log('Input not found');
-    return false;
-  } else if (!path.extname(input)) {
-    input += '/main.js';
-  }
-  console.log(pkg);
-
-  let name = pkg.name || pack.name;
-
-  if (!path.extname(output)) {
-    output += `/${name}.min.js`;
-  }
-  let dir = path.dirname(output);
-  exec('rm -rf ' + dir);
-  exec('mkdir ' + dir);
-
-  let i = path.normalize(input);
-  let o = path.normalize(output);
-
-  return minify(i, o, pkg);
-};
-
-prepare('../test/data/a.js', '../test/data/b.js', {
-  name: 'gcc-test',
-  version: '1.2.3',
-  repository: {
-    type: 'git',
-    url: 'https://github.com/ndaidong/gcc-min'
-  },
-  author: '@ndaidong',
-  license: 'MIT'
-});
-
 module.exports = {
-  minify,
-  prepare
+  compile
 };
---
+++ test/data/output/production.js
@@ -0,0 +1,9 @@
+/**
+ * gcc-test
+ * v1.2.3
+ * built: Tue, 13 Sep 2016 04:46:42 GMT
+ * git: https://github.com/ndaidong/gcc-min
+ * author: @ndaidong
+ * License: MIT
+**/
+;var add=function(a,b){return a+b},sub=function(a,b){return a-b};module.exports={add:add,sub:sub};
